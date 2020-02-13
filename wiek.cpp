b574e1077eb7ef6d3af624e3282d0f0ce457aa9d
young11 <50494436+young11@users.noreply.github.com>
2020-02-13 13:23:49 +0100

    Create wiek.cpp
    becz00nia

---
+++ wiek.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+int wiek;
+cout<<"Podaj swoj wiek: ";
+cin>>wiek;
+if(wiek>=18)
+cout<<"Jestes pelnoletni";
+else
+cout<<"Nie jestes Pelnoletni";
+return 0;
+}
