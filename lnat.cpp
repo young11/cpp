bd13684bf1f99768f8c856bf78d54b26ca1bdcd5
young11 <50494436+young11@users.noreply.github.com>
2020-02-13 13:26:14 +0100

    Create lnat.cpp
    wiaderko

---
+++ lnat.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+int s;
+cout<<"Podaj N: ";
+cin>>n;
+s=(n*(n+1)/2);
+cout<"s ma"<<n<<"poczatkujaczych l. nat. wynosi:"<<s;
+return 0;
+}
